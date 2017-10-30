e386356ce37d07730cdf03e38a38d48789f31852
Arun Kayathi <arunreddy.kayathi@gmail.com>
2017-10-30 16:13:04 -0400

    change  header subtitle

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export const Header =()=> (
         <div className="header">
             <div className="container">
                 <h1 className="header__title">Wishy-Washy</h1>
-                <p className="header__subtitle">Let allow computer to choose your decison :)</p>
+                <p className="header__subtitle">Turn your Indecision to a decison :)</p>
             </div>
             
         </div>
